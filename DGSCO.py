47c1d80ff4ce753007b7f0e942119f241c099eb0
mosafari <m.safari2680@gmail.com>
2023-07-19 08:25:36 +0330

    initialize the MongoClient

--- DGSCO.py
+++ DGSCO.py
@@ -1,11 +1,13 @@
 # import
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 # instantiate the FastAPI object
 app = FastAPI()
 
 # initialize the MongoClient
-
+client = MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
 
 # create user model to create and find users
 
