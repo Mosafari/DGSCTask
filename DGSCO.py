bdabe55c3950683b22ac6a4f3ca4212e52122a4c
mosafari <m.safari2680@gmail.com>
2023-07-19 07:46:41 +0330

    create base structure

---
+++ DGSCO.py
@@ -0,0 +1,28 @@
+# import
+
+
+# instantiate the FastAPI object
+
+
+# initialize the MongoClient
+
+
+# create user model to create and find users
+
+
+# signup
+
+
+# Login
+
+
+# Twitter Login
+
+
+# logout from twitter
+
+
+# Find all Followers
+
+
+# Run App
