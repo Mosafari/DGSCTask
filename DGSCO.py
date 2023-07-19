ff0d3adfc692139ce6a2c9abadd97fd65432dad4
mosafari <m.safari2680@gmail.com>
2023-07-19 08:27:18 +0330

    create user model to create and find users

--- DGSCO.py
+++ DGSCO.py
@@ -10,7 +10,27 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["mydatabase"]
 
 # create user model to create and find users
+class UserModel:
+    collection = db["users"]  # MongoDB collection for storing users
 
+    @classmethod # create a user
+    def create_user(cls, username: str, password: str) -> str:
+        user = {"username": username, "password": password}
+        result = cls.collection.insert_one(user)
+        return str(result.inserted_id)
+
+    @classmethod # check if user already exists
+    def find_user(cls, username: str) :
+        return cls.collection.find_one({"username": username})
+    
+    @classmethod # check password with username
+    def passcheck(cls, username: str, password: str) :
+        return cls.collection.find_one({"username": username, "password": password})
+    
+    @classmethod # find user by id
+    def find_user_by_id(cls, user_id: str):
+        user = cls.collection.find_one({"_id": ObjectId(user_id)})
+        return user
 
 # signup
 @app.post("/signup")
